34c8448a430bf7222edb6273f0159827fd9343c1
danielosw <65432065+danielosw@users.noreply.github.com>
2024-03-18 08:47:28 -0500

    add nvim-lightbulb

--- init.lua
+++ init.lua
@@ -339,7 +339,9 @@ if vim.g.neovide then
     vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
     vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
-
+require("nvim-lightbulb").setup({
+  autocmd = { enabled = true }
+})
 -- Allow clipboard copy paste in neovim
 vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', {noremap = true, silent = true})
---
+++ lua/plugins/nvim-lightbulb.lua
@@ -0,0 +1,1 @@
+return { 'kosayoda/nvim-lightbulb' }
