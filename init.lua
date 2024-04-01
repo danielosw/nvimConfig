cf95de6dd5a8d15b60b32aa7eb742df34245e48f
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-04-01 08:48:41 -0500

    add rainbow delimiters and lspkind.

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ function Iswindows()
 		return false
 	end
 end
-
 if Iswindows() then
 	vim.o.shell = "pwsh.exe"
 end
@@ -239,7 +238,7 @@ require("swenv").setup({
 
 -- nvim cmp config
 local cmp = require("cmp")
-
+local lspkind = require('lspkind')
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
@@ -269,6 +268,22 @@ cmp.setup({
 		-- { name = 'ultisnips' }, -- For ultisnips users.
 		--{ name = 'snippy' }, -- For snippy users.
 	}, { { name = "buffer" } }),
+	formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                     -- can also be a function to dynamically calculate max width such as 
+                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  }
 })
 
 -- Set configuration for specific filetype.
