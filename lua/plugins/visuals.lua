57d2c423b60bd321c26d336edf9414363ccac7e1
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-04-02 09:45:34 -0500

    switch back to bufferline for preformence

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -11,20 +11,16 @@ return {
 		},
 	},
 	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					separator_style = "slant",
+				},
+			})
 		end,
-		opts = {
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
 	},
 	{ "anuvyklack/animation.nvim", cond = IsNotNeovide },
 	{
