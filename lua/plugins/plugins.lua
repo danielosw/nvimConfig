efcef86e9d403a9f0c07f1cfa683459e2282c217
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-04-10 12:54:25 -0500

    lazy load bacon and swenv

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -9,8 +9,27 @@ end
 return {
 	"nvim-lua/plenary.nvim",
 	"mhartington/formatter.nvim",
-	"Canop/nvim-bacon",
-	"AckslD/swenv.nvim",
+	{"Canop/nvim-bacon",
+	ft = "rust"
+	},
+	{"AckslD/swenv.nvim",
+	ft = "python",
+	config = function ()
+	require("swenv").setup({
+	-- Should return a list of tables with a `name` and a `path` entry each.
+	-- Gets the argument `venvs_path` set below.
+	-- By default just lists the entries in `venvs_path`.
+	get_venvs = function(venvs_path)
+		return require("swenv.api").get_venvs(venvs_path)
+	end,
+	-- Path passed to `get_venvs`.
+	venvs_path = vim.fn.expand("~/venvs"),
+	-- Something to do after setting an environment, for example call vim.cmd.LspRestart
+	post_set_venv = nil,
+})
+		
+	end,
+	},
 	{ "stevearc/dressing.nvim", opts = {} },
 	"williamboman/mason.nvim",
 	{
