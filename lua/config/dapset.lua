ea9b0b9c68aa36efb78082e89676452e6823e7da
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-04-12 12:03:51 -0500

    add hopefully working kotlin debugging

--- lua/config/dapset.lua
+++ lua/config/dapset.lua
@@ -51,7 +51,39 @@ dap.configurations.cpp = {
 	},
 }
 dap.configurations.c = dap.configurations.cpp
-
+dap.configurations.kotlin = {
+{
+        type = "kotlin",
+        request = "launch",
+        name = "This file",
+        -- may differ, when in doubt, whatever your project structure may be,
+        -- it has to correspond to the class file located at `build/classes/`
+        -- and of course you have to build before you debug
+        mainClass = function()
+            local root = vim.fs.find("src", { path = vim.uv.cwd(), upward = true, stop = vim.env.HOME })[1] or ""
+            local fname = vim.api.nvim_buf_get_name(0)
+            -- src/main/kotlin/websearch/Main.kt -> websearch.MainKt
+            return fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1)
+        end,
+        projectRoot = "${workspaceFolder}",
+        jsonLogFile = "",
+        enableJsonLogging = false,
+    },
+    {
+        -- Use this for unit tests
+        -- First, run 
+        -- ./gradlew --info cleanTest test --debug-jvm
+        -- then attach the debugger to it
+        type = "kotlin",
+        request = "attach",
+        name = "Attach to debugging session",
+        port = 5005,
+        args = {},
+        projectRoot = vim.fn.getcwd,
+        hostName = "localhost",
+        timeout = 2000,
+    },
+}
 dap.configurations.rust = dap.configurations.cpp
 function setupDap(temp)
 	if temp == "debugpy" then
@@ -99,6 +131,13 @@ function setupDap(temp)
 				command = templldb:get_install_path() .. catpath,
 				args = { "--port", "${port}" },
 			},
+		}
+	end
+	if temp == "kotlin-debug-adapter" then
+		dap.adapters.kotlin = {
+			type = "executable",
+			command = "kotlin-debug-adapter",
+			options = { auto_continue_if_many_stopped = false },
 		}
 	end
 end
