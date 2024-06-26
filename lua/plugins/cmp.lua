a8dc29759d1d1eba803381a2ee366acb36e112fc
danielosw <65432065+danielosw@users.noreply.github.com>
2024-06-26 12:28:47 -0500

    switch to installing and updating jsregexp with luarocks

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,12 +13,12 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 		build = function()
-			if package.config:sub(1, 1) == "\\" then
-				return
-			else
-				return "make install_jsregexp"
+		-- check if luarocks is installed
+		 if vim.fn.exepath("luarocks") ~= "" then
+				-- install or update jsregexp locally
+				vim.cmd("te luarocks --local install jsregexp")
 			end
-		end,
+			end,
 		dependencies = { "rafamadriz/friendly-snippets", "molleweide/LuaSnip-snippets.nvim" },
 	},
 }
