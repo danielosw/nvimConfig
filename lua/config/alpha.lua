af25bb13d17df8f66766b43f32213f3d43b6bd49
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-11-20 14:47:36 -0600

    add live grep

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -17,6 +17,7 @@ dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
     dashboard.button( "f", "󰱼  > Find file", ":Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+	dashboard.button( "w", "  > Find text", ":Telescope live_grep<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
     dashboard.button( "q", "󰰲  > Quit NVIM", ":qa<CR>"),
 }
