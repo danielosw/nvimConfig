386a8fe606afb2c4bce4777d5877c7353dfc0733
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-11-20 14:29:30 -0600

    fix broken symbols

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -13,10 +13,10 @@ dashboard.section.header.val = {
     "                                                     ",
 }dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "f", "󰱼  > Find file", ":Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    dashboard.button( "q", "󰰲  > Quit NVIM", ":qa<CR>"),
 }
 alpha.setup(dashboard.opts)
 vim.cmd([[
