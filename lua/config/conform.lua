2684480e695a419d6d02494deaf9ac9a580c77a9
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-12-05 15:29:14 -0600

    have ruff_fix

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -6,17 +6,30 @@ local haveformat = function(bufnr, formatter)
 		return { lsp_format = "fallback" }
 	end
 end
+local haveRuff = function(bufnr)
+	local toReturn = {}
+	if conform.get_formatter_info("ruff_format", bufnr).available then
+		toReturn[#toReturn + 1] = "ruff_format"
+	end
+	if conform.get_formatter_info("ruff_fix", bufnr).available then
+		toReturn[#toReturn + 1] = "ruff_fix"
+	end
+	if #toReturn == 0 then
+		return { lsp_format = "fallback" }
+	end
+	return toReturn
+end
 conform.setup({
 	formatters_by_ft = {
 		lua = function(bufnr)
-				return haveformat("stylua", bufnr)
+			return haveformat("stylua", bufnr)
 		end,
 		rust = function(bufnr)
-				return haveformat("rustfmt", bufnr)
+			return haveformat("rustfmt", bufnr)
 		end,
 
 		python = function(bufnr)
-				return haveformat("ruff_format", bufnr)
+			return haveRuff(bufnr)
 		end,
 		typescript = function(bufnr)
 			return haveformat(bufnr, "biome")
@@ -43,8 +56,8 @@ conform.setup({
 	["*"] = { "codespell" },
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
