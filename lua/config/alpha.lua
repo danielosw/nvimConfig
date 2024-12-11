6c088ece13568c8cc0b7317b00d6dc79f939a9d3
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-12-11 14:43:23 -0600

    add open folder

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -35,6 +35,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
 	dashboard.button("w", "  > Find text", ":Telescope live_grep<CR>"),
 	dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | NvimTreeOpen | wincmd k | pwd<CR>"),
+	dashboard.button("t", "󱃩  > Open folder", ":e .| NvimTreeOpen | wincmd k | pwd<CR>"),
 	dashboard.button("q", "󰰲  > Quit NVIM", ":qa<CR>"),
 }
 alpha.setup(dashboard.opts)
